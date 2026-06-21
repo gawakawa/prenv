bfc04436b74da4aadb4f1070991e98c931112007
gawakawa <mashurakiryu@icloud.com>
2026-06-21 20:14:48 +0900

    🎉 Initial Commit

---
+++ flakes/devShells.nix
@@ -0,0 +1,21 @@
+_: {
+  perSystem =
+    { config, pkgs, ... }:
+    let
+      devPackages =
+        config.pre-commit.settings.enabledPackages
+        ++ (with pkgs; [
+          opentofu
+          terraform-docs
+        ]);
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = devPackages;
+
+        shellHook = ''
+          ${config.pre-commit.shellHook}
+        '';
+      };
+    };
+}
